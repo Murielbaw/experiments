775e1cbd2152026748e388d194c2a4471df62290
robertbutacu <robertbutacu@gmail.com>
2022-04-06 12:30:41 +0100

    Small stylistic adjustments

--- project/hahaha.py
+++ project/hahaha.py
@@ -11,13 +11,13 @@ def compound_interest():
 window = tk.Tk()
 window.title("How much you owe me")
 window.resizable(width=False, height=False)
-greetings = tk.Label(text="Enter the price of your stupid shoes+ the stupid wooden stuff which is 5500, \nclick on the arrow button, \nyou will get the final amount, \nyou don't need to know how the amount is calculated :) ",background="HotPink")
+greetings = tk.Label(text="Enter the price of your beautiful shoes+ the amazing shoe trees which is 5500, \nclick on the arrow button, \nyou will get the final amount, \nyou don't need to know how the amount is calculated :) ",background="Green")
 
 
 frm_entry = tk.Frame(master=window)
-lbl_principle=tk.Label(master=frm_entry, text="Principle",background="HotPink")
+lbl_principle=tk.Label(master=frm_entry, text="Principle",background="Green")
 ent_principle = tk.Entry(master=frm_entry, width=10)
-lbl_currency = tk.Label(master=frm_entry, text="£",background="HotPink")
+lbl_currency = tk.Label(master=frm_entry, text="£",background="Green")
 
 
 greetings.grid(row=0, column=0, sticky="w")
@@ -28,16 +28,16 @@ lbl_currency.grid(row=1, column=2)
 btn_convert = tk.Button(
     master=window,
     text="\N{RIGHTWARDS BLACK ARROW}",
-    background="HotPink",
+    background="Green",
     command=compound_interest
 )
-lbl_result = tk.Label(master=window,background="HotPink")
+lbl_result = tk.Label(master=window,background="Green")
 
 
-ending=tk.Label(master=window, background="HotPink",text="Transfer the result amount to the following account by the end of today, otherwise you are dead, biu :)")
-account_name=tk.Label(master=window, text="Name:Aiwei Bian",background="HotPink")
-account_Sc=tk.Label(master=window, text="Sort Code: 30-90-89",background="HotPink")
-account_No=tk.Label(master=window, text="Account: 76971860",background="HotPink")
+ending=tk.Label(master=window, background="Green",text="Transfer the result amount to the following account by the end of today, otherwise you are dead, biu :)")
+account_name=tk.Label(master=window, text="Name:Aiwei Bian",background="Green")
+account_Sc=tk.Label(master=window, text="Sort Code: 30-90-89",background="Green")
+account_No=tk.Label(master=window, text="Account: 76971860",background="Green")
 
 frm_entry.grid(row=1, column=0, padx=10)
 btn_convert.grid(row=1, column=1, pady=10)
